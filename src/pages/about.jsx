1a68da29037cb661b7188141ab5da18a8eea7db5
Frank151001 <124334022+FranciscoIbarraG@users.noreply.github.com>
2023-03-30 23:07:32 -0700

    Final Proyect

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,7 @@ import {useState} from 'react';
 
 function About(){
     const[displayEmail,setDisplayEmail] = useState(false);
+
     function toggleEmail(){
         console.log("Hello");
         setDisplayEmail(true);
@@ -10,18 +11,18 @@ function About(){
 
     function getEmailSection(){
         if(displayEmail){
-            return  <h5>Franckgomez323@gmail.com</h5>
+            return(  <h5>Franckgomez323@gmail.com</h5>)
         }
         else{
-            return null;
+            return (null)
         }
     }
 
     return(
         <div className="About">
-            <h1>Francisco Ibarra</h1>
-            {getEmailSection}
             <i class="fa fa-car" aria-hidden="true"></i>
+            <h1>Francisco Ibarra</h1>
+            {getEmailSection()}
             <button className="btn btn-sm btn-dark" onClick={toggleEmail}>Show my Email</button>
         </div>
     );
